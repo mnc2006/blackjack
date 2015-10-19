e3d4941c159802bca75fd7bd7fab992de32124c9
bendoane <ben.doane@gmail.com>
2015-10-19 15:55:04 -0400

    I have changed the value of the cards for you values now match blackjack, the game added shufffle and draw methods

--- card.rb
+++ card.rb
@@ -2,25 +2,28 @@ class Card
 
   attr_accessor :suit, :face, :value
 
-def initialize(face,suit)
-  self.suit = suit
-  self.face = face
-  if face == "Ace"
-    self.value = 14
-  elsif face == "King"
-    self.value = 13
-  elsif face == "Queen"
-    self.value = 12
-  elsif face == "Jack"
-    self.value = 11
+  def initialize(face,suit)
+    self.suit = suit
+    self.face = face
+    if face == "Ace"
+      self.value = 11
+    elsif face == "King"
+      self.value = 10
+    elsif face == "Queen"
+      self.value = 10
+    elsif face == "Jack"
+      self.value = 10
 
-  else
-    self.value = face.to_i
+    else
+      self.value = face.to_i
+    end
   end
-end
 
-def >(card)
-  self.value > card.value
+  def >(card)
+    self.value > card.value
+  end
 
-
+  def <(card)
+    self.value > card.value
+  end
 end
